3e6044eafedf340103a6a3a7c98cabaf0c223ef3
meghana-zodiac <meghana@zodiachrc.com>
2026-08-12 18:27:22 +0530

    Fix formatting and structure in apollo-prospector.tsx

--- src/components/crm/apollo-prospector.tsx
+++ src/components/crm/apollo-prospector.tsx
@@ -109,10 +109,12 @@ export function ApolloProspector({ target }: { target: Target }) {
         </div>
 
         <div className="mt-4 grid gap-3 sm:grid-cols-2 xl:grid-cols-4">
+ <div className="mt-4 grid gap-3 sm:grid-cols-2 xl:grid-cols-4">
   <Field label="Job title" value={jobTitle} onChange={setJobTitle} placeholder="HR" />
   <Field label="Company name" value={companyName} onChange={setCompanyName} placeholder="Vivo" />
   <Field label="Location" value={location} onChange={setLocation} placeholder="Bengaluru, India" />
   <Field label="Industry" value={industry} onChange={setIndustry} placeholder="Information Technology" />
+</div>
 </div>
 
         <div className="mt-4">
