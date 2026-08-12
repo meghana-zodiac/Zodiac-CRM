2cc601cc3ec3982ff59a489cbebf6ee779e84dbd
meghana-zodiac <meghana@zodiachrc.com>
2026-08-12 18:24:16 +0530

    Update apollo-prospector.tsx

--- src/components/crm/apollo-prospector.tsx
+++ src/components/crm/apollo-prospector.tsx
@@ -109,11 +109,11 @@ export function ApolloProspector({ target }: { target: Target }) {
         </div>
 
         <div className="mt-4 grid gap-3 sm:grid-cols-2 xl:grid-cols-4">
-          <Field label="Job title" value={jobTitle} onChange={setJobTitle} placeholder="HR" />
-          <Field label="Company name" value={companyName} onChange={setCompanyName} placeholder="Vivo" />
-          <Field label="Location" value={location} onChange={setLocation} placeholder="Bengaluru, India" />
-          <Field label="Industry" value={industry} onChange={setIndustry} placeholder="Information Technology" />
-        </div>
+  <Field label="Job title" value={jobTitle} onChange={setJobTitle} placeholder="HR" />
+  <Field label="Company name" value={companyName} onChange={setCompanyName} placeholder="Vivo" />
+  <Field label="Location" value={location} onChange={setLocation} placeholder="Bengaluru, India" />
+  <Field label="Industry" value={industry} onChange={setIndustry} placeholder="Information Technology" />
+</div>
 
         <div className="mt-4">
           <Button size="sm" onClick={() => search.mutate()} disabled={search.isPending}>
